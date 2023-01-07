6f588e69f203bf304b2addda345043e792b52065
Reid-Cui7 <reid.cmj@gmail.com>
2023-01-07 22:12:35 +0800

    create app

--- acapp/settings.py
+++ acapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b00vl(b!yd^wte0=iekqeysyv)6mq1c4whhv98lz(cbb%*k6kr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["121.4.80.186"]
+ALLOWED_HOSTS = ["121.4.80.186", "app1328.acapp.acwing.com.cn"]
 
 
 # Application definition
--- game/static/js/src/playground/player/zbase.js
+++ game/static/js/src/playground/player/zbase.js
@@ -38,13 +38,14 @@ class Player extends AcGameObject {
             return false;
         });
         this.playground.game_map.$canvas.mousedown(function(e) {
+            const rect = outer.ctx.canvas.getBoundingClientRect();
             if (e.which === 3) {
-                outer.move_to(e.clientX, e.clientY);
+                outer.move_to(e.clientX - rect.left, e.clientY - rect.top);
             } else if (e.which === 1) {
                 // console.log(1);
                 if (outer.cur_skill === "fireball") {
                     // console.log("fireball");
-                    outer.shoot_fireball(e.clientX, e.clientY);
+                    outer.shoot_fireball(e.clientX - rect.left, e.clientY - rect.top);
                 }
 
                 outer.cur_skill = null;
